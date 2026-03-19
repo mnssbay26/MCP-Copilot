7599ad00c00a3596416b3bf0abf6beadf3376db4
Enrique Meneses <enrique.meneses@bayer.com>
2026-03-18 20:13:56 -0600

    ci: add GitHub Actions workflow for ECR build and push

--- src/shared/aps/endpoints.ts
+++ src/shared/aps/endpoints.ts
@@ -1,5 +1,6 @@
 const APS_BASE_URL = "https://developer.api.autodesk.com";
 
+//Comment
 export const APS_OAUTH_AUTHORIZE_URL = `${APS_BASE_URL}/authentication/v2/authorize`;
 export const APS_OAUTH_TOKEN_URL = `${APS_BASE_URL}/authentication/v2/token`;
 export const APS_CONSTRUCTION_ADMIN_BASE_URL = `${APS_BASE_URL}/construction/admin/v1`;
